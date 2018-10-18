89e96f267597a07fab73b2068fb1c08021481cd7
Paul Walsh <twentytwentyk@live.com>
2018-10-18 10:11:27 -0400

    converting all notes to objects

--- src/components/Scales.js
+++ src/components/Scales.js
@@ -14,6 +14,7 @@ class Scales extends Component {
     super();
     this.state = {
       notes: [],
+      notes2: {0: ''},
       value: '',
     }
 
@@ -21,7 +22,20 @@ class Scales extends Component {
   }
   
   changeKey(event){
-    let notes = ["C", "Db", "D", "Eb", "E", "F", "F#", "Gb", "Ab", "A", "Bb", "B"]
+    let notes = [
+      "C", 
+      "Db", 
+      "D", 
+      "Eb", 
+      "E", 
+      "F", 
+      "F#", 
+      "Gb", 
+      "Ab", 
+      "A", 
+      "Bb", 
+      "B",
+    ]    
     const e = event.target.value
     let chunk1 = notes.slice(0, e)
     let chunk2 = notes.slice(e, notes.length)
@@ -31,22 +45,42 @@ class Scales extends Component {
     })
   }
 
+  changeKey2(event){
+    let notes2 = [
+      {0: "C"}, 
+      {1: "Db"}, 
+      {2: "D"}, 
+      {3: "Eb"}, 
+      {4: "E"}, 
+      {5: "F"}, 
+      {6: "F#"}, 
+      {7: "Gb"}, 
+      {8: "Ab"}, 
+      {9: "A"}, 
+      {10: "Bb"}, 
+      {11: "B"},
+    ]
+    const e = event.target.value
+    let chunk1 = notes2.slice(0, e)
+    let chunk2 = notes2.slice(e, notes2.length)
+    let newKey = chunk2.concat(chunk1)
+    this.setState({
+      notes2: newKey,
+    })
+  }
+
+
   render() {
     const notes = this.state.notes;
+    const notes2 = this.state.notes;
+
     const key = this.state.value
-    const buildScale = (a,b,c,d,e,f,g) => [
-      notes[a],
-      notes[b],
-      notes[c],
-      notes[d],
-      notes[e],
-      notes[f],
-      notes[g],
-    ]
     
     const triads = (scale) => {
       // create a new array, set a starting index, reach the end of the array, return to the first index and stop the loop at the initial array.
-
+      let closed = [[scale[0], scale[2], scale[4]],
+      [scale[1], scale[3], scale[5]]]
+      
       let arr = [
         [scale[0], scale[2], scale[4]],
         [scale[1], scale[3], scale[5]],
@@ -57,12 +91,38 @@ class Scales extends Component {
         [scale[6], scale[1], scale[3]]
       ]
       return arr.map((item, key )=> 
-          <div>
+      <div>
             <ul key={key}>{item.join(' - ')}</ul>
           </div>
       )
     }
 
+    const buildScale = (a,b,c,d,e,f,g) => [
+      notes[a],
+      notes[b],
+      notes[c],
+      notes[d],
+      notes[e],
+      notes[f],
+      notes[g],
+    ]
+
+    const buildScale2 = ({a,b,c,d,e,f,g}) => { 
+      const arr = [
+        {a: notes2[a]},
+        {b: notes2[b]},
+        {c: notes2[c]},
+        {d: notes2[d]},
+        {e: notes2[e]},
+        {f: notes2[f]},
+        {g: notes2[g]},
+      ]
+      const degrees = arr.map(obj => Object.values(obj).map(x => x))
+      const scale = Object.keys(arr);
+      console.log(arr, degrees, scale)
+    }
+
+    const major2 = buildScale({'1': 0},{'2': 2},{'3': 4},{'4': 5},{'5': 7},{'6': 9},{'7': 11})
     const major = buildScale(0,2,4,5,7,9,11)
     const harmonicMinor = buildScale(0,2,3,5,7,8,11)
 
@@ -76,7 +136,7 @@ class Scales extends Component {
       arr.shift()
       return arr.map(x => <ul>{x.join(' - ')}</ul>)
     }
-
+    buildScale2(major2)
     return(
       <div>
         <div>
@@ -99,6 +159,8 @@ class Scales extends Component {
           <option value='10'>Bb</option>
           <option value='11'>B</option>
         </select>
+        {/* <h1>{buildScale2(major2)}</h1> */}
+
         <h1>{key.toString} Major</h1>
         <h1>{major.join(' - ')}</h1>
         <h1>{modes(major)}</h1>
