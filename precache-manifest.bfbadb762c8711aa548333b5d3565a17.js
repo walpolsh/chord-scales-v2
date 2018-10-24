7cad050e4d16cc9fd9738e367c6b79f4056aeb37
Paul Walsh <twentytwentyk@live.com>
2018-10-24 18:12:44 -0400

    Updates

---
+++ precache-manifest.bfbadb762c8711aa548333b5d3565a17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b0acc065d5460d6c7ca",
+    "url": "/chord-scales/static/js/runtime~main.7b0acc06.js"
+  },
+  {
+    "revision": "4f97ec045764a1cfebd0",
+    "url": "/chord-scales/static/js/main.4f97ec04.chunk.js"
+  },
+  {
+    "revision": "bd11c0b82c83630c08cf",
+    "url": "/chord-scales/static/js/1.bd11c0b8.chunk.js"
+  },
+  {
+    "revision": "4f97ec045764a1cfebd0",
+    "url": "/chord-scales/static/css/main.e80306e0.chunk.css"
+  },
+  {
+    "revision": "e6cfa3d9182953f9762e0a78f9394df4",
+    "url": "/chord-scales/index.html"
+  }
+];
